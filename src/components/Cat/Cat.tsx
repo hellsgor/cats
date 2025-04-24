e1fe9febd7cfc9da3af14ba56e3d8d46794bdbfd
Igor Storchevoy <hellsgor@gmail.com>
2025-04-24 12:42:09 +0400

    Feat[Cat]: crated Cat component with scss

---
+++ src/components/Cat/Cat.tsx
@@ -0,0 +1,13 @@
+import "./cat.scss";
+
+export function Cat() {
+  return (
+    <div className="cat">
+      <img
+        className="cat__image"
+        src="https://cdn2.thecatapi.com/images/MTgxNzkxMQ.jpg"
+        alt="cat"
+      />
+    </div>
+  );
+}
