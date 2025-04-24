ba90b2b4fb8e57e7476a20ed0e7fadc25eeb375d
Igor Storchevoy <hellsgor@gmail.com>
2025-04-24 15:01:52 +0400

    Feat[App]: integrate random cat fetching with auto-refresh and error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import "./App.css";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Card from "./components/Card";
 import Checkbox from "./components/Checkbox";
 import Button from "./components/Button";
 import Cat from "./components/Cat";
+import { useQuery } from "@tanstack/react-query";
+import { catsApi } from "./api/cats";
+import { queryClient } from "./api/query-client";
 
 export default function App() {
   const [isEnabled, setIsEnabled] = useState<boolean>(true);
-  const [refresh, setRefresh] = useState({
+  const [autoRefresh, setAutoRefresh] = useState({
     auto: false,
     delay: 5000,
   });
 
+  const { data, error, isFetching } = useQuery({
+    queryKey: ["cats", "random"],
+    queryFn: (meta) => catsApi.getRandomCat(meta),
+    enabled: isEnabled,
+    refetchOnWindowFocus: false,
+    refetchInterval: autoRefresh.auto ? autoRefresh.delay : false,
+  });
+
+  const handleClick = useCallback(
+    () =>
+      queryClient.invalidateQueries({
+        queryKey: ["cats", "random"],
+      }),
+    [],
+  );
   return (
     <Card>
       <Checkbox
@@ -20,14 +38,16 @@ export default function App() {
         onChange={() => setIsEnabled(!isEnabled)}
       />
       <Checkbox
-        text={`Auto-refresh every ${refresh.delay / 1000} seconds`}
-        checked={refresh.auto}
-        onChange={() => setRefresh({ ...refresh, auto: !refresh.auto })}
+        text={`Auto-refresh every ${autoRefresh.delay / 1000} seconds`}
+        checked={autoRefresh.auto}
+        onChange={() =>
+          setAutoRefresh({ ...autoRefresh, auto: !autoRefresh.auto })
+        }
       />
-      <Button wide={true} disabled={!isEnabled}>
+      <Button wide={true} disabled={!isEnabled} onClick={handleClick}>
         Get cat
       </Button>
-      <Cat />
+      <Cat data={data} isFetching={isFetching} error={error || undefined} />
     </Card>
   );
 }
